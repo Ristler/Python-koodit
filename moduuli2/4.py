3b9571150046a25f9a386d7b3e9276fe8b2bb81f
Daniel <100704382+Ristler@users.noreply.github.com>
2024-08-21 00:00:28 +0300

    Tehtäviä done progress

---
+++ moduuli2/4.py
@@ -0,0 +1,19 @@
+import math
+
+print('Anna kolme lukua, lasken lukujen summan, tulon ja keskiarvon.')
+print('-------------------------------------------------------------')
+
+luku1 = int(input("Ensimmäinen luku: "))
+luku2 = int(input("Toinen luku: "))
+luku3 = int(input("kolmas luku: "))
+print('-------------------------------------------------------------')
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+
+print('Lukujen summa on:', summa)
+print('Lukujen tulo on:', tulo)
+print('Lukujen keskiarvo on:', keskiarvo)
+print('-------------------------------------------------------------')
+
---
+++ moduuli2/5.py
@@ -0,0 +1,21 @@
+
+leiviska = float(input("Anna leiviskät."))
+naula = float(input("Anna naulat"))
+luoti = float(input("Anna luoti"))
+
+luotitoG = luoti * 13.3
+naulatoG = naula * 425.6
+leiviskatoG = leiviska * 8512
+
+total = luotitoG + naulatoG + leiviskatoG
+
+kg = int(total / 1000)
+g = total % 1000
+
+print("Massa nykymittojen mukaan:")
+print(kg,f"kilogrammaa ja{g:7.2f} grammaa.")
+
+
+
+
+
