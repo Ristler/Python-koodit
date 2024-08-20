0c3463c1593cc7edb0b3a565e813aca2be53be81
Daniel <100704382+Ristler@users.noreply.github.com>
2024-08-21 00:41:02 +0300

    Tehtäviä done progress

---
+++ moduuli2/6.py
@@ -0,0 +1,8 @@
+import random
+
+print("3 number random generated code:")
+print(random.randint(0, 9), random.randint(0, 9), random.randint(0, 9))
+print("-------------------------------")
+print("4 number random generated code:")
+print(random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),)
+print("-------------------------------")
