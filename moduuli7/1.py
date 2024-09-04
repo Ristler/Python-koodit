03c896fc405983e8ae85b0bc7a99308281dea91c
Daniel <100704382+Ristler@users.noreply.github.com>
2024-09-04 14:23:20 +0300

    Tehtäviä done progress

--- moduuli7/1.py
+++ moduuli7/1.py
@@ -1,33 +1,21 @@
-#Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron,
-#jonka jälkeen ohjelma tulostaa sitä vastaavan vuodenajan
-#(kevät, kesä, syksy, talvi). Tallenna ohjelmassasi kuukausia vastaavat
-#vuodenajat merkkijonoina
-#monikkotietorakenteeseen.
-# Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten,
-#että joulukuu on ensimmäinen talvikuukausi.
 
 kuukaudet = ("tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu")
 kuukausinro = int(input("Anna kuukauden numero: "))
-vuodenaika = "tammikuu"
-
 
 while kuukausinro != 0:
-
-    if kuukausinro <= 3:
+    if kuukausinro == 3 or kuukausinro == 4 or kuukausinro == 5:
         print("Kevät")
         kuukausinro = int(input("Anna kuukauden numero: "))
 
-
-    elif kuukausinro <= 6:
+    elif kuukausinro == 6 or kuukausinro == 7 or kuukausinro == 8:
         print("Kesä")
         kuukausinro = int(input("Anna kuukauden numero: "))
 
-
-    elif kuukausinro <= 9:
+    elif kuukausinro == 9 or kuukausinro == 10 or kuukausinro == 11:
         print("Syksy")
         kuukausinro = int(input("Anna kuukauden numero: "))
 
-    elif kuukausinro <= 12:
+    elif kuukausinro == 2 or kuukausinro == 1 or kuukausinro == 12 :
         print("Talvi")
         kuukausinro = int(input("Anna kuukauden numero: "))
 
