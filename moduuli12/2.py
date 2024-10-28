0a22f279100e9aa6b87380256e9a6ce1704ad510
Daniel <100704382+Ristler@users.noreply.github.com>
2024-10-28 14:44:56 +0200

    Tehtäviä done progress

---
+++ moduuli12/2.py
@@ -0,0 +1,13 @@
+
+import requests
+
+paikkakunta = input("Anna paikkakunta: ")
+pyyntö = f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta}&appid=a89956b7488968e2882bae86106eb1b6&units=metric"
+
+try:
+    vastaus = requests.get(pyyntö)
+    if vastaus.status_code==200:
+        json_vastaus = vastaus.json()
+        print(json_vastaus["main"]["temp"], "°C")
+except requests.exceptions.RequestException as e:
+    print ("Hakua ei voitu suorittaa.")
