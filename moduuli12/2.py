355e9654ecb72436a181b3280c322a29e4dc4ebc
Daniel <100704382+Ristler@users.noreply.github.com>
2024-10-28 14:50:48 +0200

    Tehtäviä done progress

--- moduuli12/2.py
+++ moduuli12/2.py
@@ -9,5 +9,6 @@ try:
     if vastaus.status_code==200:
         json_vastaus = vastaus.json()
         print(json_vastaus["main"]["temp"], "°C")
+        print(json_vastaus["weather"][0]["main"])
 except requests.exceptions.RequestException as e:
     print ("Hakua ei voitu suorittaa.")
